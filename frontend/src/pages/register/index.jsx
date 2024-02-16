91a7b97780181b6cd224e82034cc2bcfe15580b7
Gilmar Alves <GilmarAlves914@gmail.com>
2024-02-15 22:03:30 -0300

    FEAT: AXIOS/API salvando usuários

--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -1,13 +1,33 @@
 import { Main, Body, Logo, Spacing, CreateAcc } from "./styles"
 import { Input, Button } from "../../components";
 import { useNavigate } from "react-router";
+import { useState } from "react";
+import axios from "axios";
+
+
 
 const Register = () => {
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
+
   const navigate = useNavigate();
+
+  const save = async (ev) => {
+    ev.preventDefault();
+    await axios.post("http://localhost:3001/user", data);
+    alert("Usuario criado com sucesso");
+
+  }
+
+
   return (
     <Main>
       <Body>
-        <form>
+        <form onSubmit={save}>
 
 
           <Logo>Gerenciador Financeiro</Logo>
@@ -15,6 +35,10 @@ const Register = () => {
             label="Nome"
             placeholder="Digite o nome"
             required={true}
+            value={data.name}
+            onChange={(ev) => setData({
+              ...data, name: ev.target.value
+            })}
           />
           <Spacing />
           <Input
@@ -22,6 +46,10 @@ const Register = () => {
             placeholder="Digite o email"
             required={true}
             type="email"
+            value={data.email}
+            onChange={(ev) => setData({
+              ...data, email: ev.target.value
+            })}
           />
           <Spacing />
           <Input
@@ -29,6 +57,10 @@ const Register = () => {
             placeholder="Digite a senha"
             type="password"
             required={true}
+            value={data.password}
+            onChange={(ev) => setData({
+              ...data, password: ev.target.value
+            })}
           />
           <Spacing />
           <Button
